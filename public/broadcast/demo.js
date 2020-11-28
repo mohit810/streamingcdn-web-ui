2fd86754d2905614ac3bafdce543343c2a88fffe
mohit <iammohit810@gmail.com>
2020-11-28 15:31:35 +0530

    broadcaster ui update

--- public/broadcast/demo.js
+++ public/broadcast/demo.js
@@ -2,23 +2,21 @@ let divSelectRoom = document.getElementById("selectRoom")
 let inputstreamKey = document.getElementById("streamkey")
 let signalingContainer = document.getElementById('signalingContainer')
 let createSessionButton = document.getElementsByClassName('createSessionButton')
-let remoteSessionDescription = document.getElementById('remoteSessionDescription')
-let localSessionDescription = document.getElementById('localSessionDescription')
-let video1 = document.getElementById('video1')
+let videoText = document.getElementById("videoText")
 
-let streamKey, encryptedSdp, PublisherFlag, uid
+let streamKey, encryptedSdp, remoteSessionDescription
 
 /* eslint-env browser */
 var log = msg => {
-  document.getElementById('logs').innerHTML += msg + '<br>'
+    document.getElementById('logs').innerHTML += msg + '<br>'
 }
 
 const hdConstraints = {
-  audio: true,
-  video: {
-    width: { max: 1920, ideal: 1280 },
-    height: { max: 1080, ideal: 720 }
-  }
+    audio: true,
+    video: {
+        width: { max: 1920, ideal: 1280 },
+        height: { max: 1080, ideal: 720 }
+    }
 };
 
 let displayVideo = video => {
@@ -39,81 +37,71 @@ function postRequest () {
         "streamKey": streamKey
     })
     console.log(data);
-  const url = `${window.location.protocol}//${window.location.hostname}:8080/sdp`;
+    const url = `http://${window.location.protocol}//${window.location.hostname}:8080/sdp`;
     (async () => {
         const rawResponse = await fetch(url, {
-        method : "POST",
-        //body: new FormData(document.getElementById("inputform")),
-        // -- or --
-        body : data,
-        headers :{
-            'Content-Type': 'application/json'
-        }
-    });
-    const content = await rawResponse.json();
-        remoteSessionDescription.value = content.sdp
+            method : "POST",
+            //body: new FormData(document.getElementById("inputform")),
+            // -- or --
+            body : data,
+            headers :{
+                'Content-Type': 'application/json'
+            }
+        });
+        const content = await rawResponse.json();
+        remoteSessionDescription = content.sdp
         window.startSession()
-})();
+    })();
 }
 window.createSession = isPublisher => {
-  PublisherFlag = isPublisher
-  if (inputstreamKey.value === '') {
-    alert("please enter a room name.")
-  } else{
-    streamKey = inputstreamKey.value
-    let pc = new RTCPeerConnection()
-  pc.oniceconnectionstatechange = e => log(pc.iceConnectionState)
-  pc.onicecandidate = event => {
-    if (event.candidate === null) {
-      encryptedSdp = btoa(JSON.stringify(pc.localDescription))
-      localSessionDescription.value = encryptedSdp
-        postRequest();
-    }
-  }
+    if (inputstreamKey.value === '') {
+        alert("Please enter a something unique.")
+    } else{
+        streamKey = inputstreamKey.value
+        let pc = new RTCPeerConnection({
+            iceServers: [
+                {'urls': 'stun:stun.services.mozilla.com'},
+                {'urls': 'stun:stun.l.google.com.19302'}
+            ]
+        })
+        pc.oniceconnectionstatechange = e => log(pc.iceConnectionState)
+        pc.onicecandidate = event => {
+            if (event.candidate === null) {
+                encryptedSdp = btoa(JSON.stringify(pc.localDescription))
+                postRequest();
+            }
+        }
 
-  if (isPublisher) {
-      navigator.mediaDevices.getUserMedia(hdConstraints)
-          .then(stream => {
-              stream.getTracks().forEach(function(track) {
-                  pc.addTrack(track, stream);
-              });
-              displayVideo(stream);
-            pc.createOffer()
-                .then(d => {
-                  pc.setLocalDescription(d)
+        if (isPublisher) {
+            navigator.mediaDevices.getUserMedia(hdConstraints)
+                .then(stream => {
+                    videoText.style = "visibility: visible"
+                    stream.getTracks().forEach(function(track) {
+                        pc.addTrack(track, stream);
+                    });
+                    displayVideo(stream);
+                    pc.createOffer()
+                        .then(d => {
+                            pc.setLocalDescription(d)
+                        }).catch(log)
                 }).catch(log)
-          }).catch(log)
-  }else{
-      pc.addTransceiver('audio', {'direction': 'recvonly'})
-      pc.addTransceiver('video', {'direction': 'recvonly'})
-      pc.createOffer()
-          .then(d => pc.setLocalDescription(d))
-          .catch(log)
-
-      pc.ontrack = function (event) {
-        var el = video1
-        el.srcObject = event.streams[0]
-        el.autoplay = true
-        el.controls = true
-      }
-  }
-  window.startSession = () => {
-        let sd = remoteSessionDescription.value
-        if (sd === '') {
-          return alert('Session Description must not be empty')
         }
-        try {
-          pc.setRemoteDescription(new RTCSessionDescription(JSON.parse(atob(sd))))
-        } catch (e) {
-          alert(e)
+        window.startSession = () => {
+            let sd = remoteSessionDescription
+            if (sd === '') {
+                return alert('Session Description must not be empty')
+            }
+            try {
+                pc.setRemoteDescription(new RTCSessionDescription(JSON.parse(atob(sd))))
+            } catch (e) {
+                alert(e)
+            }
         }
-      }
 
-  let btns = createSessionButton
-  for (let i = 0; i < btns.length; i++) {
-    btns[i].style = 'display: none'
-  }
-  divSelectRoom.style = "display: none"
-  signalingContainer.style = 'display: block'
-}
+        let btns = createSessionButton
+        for (let i = 0; i < btns.length; i++) {
+            btns[i].style = 'display: none'
+        }
+        divSelectRoom.style = "display: none"
+    }
 }
